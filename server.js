596e41f60c0029732c7bb907de06f02ed17b92a0
chrischen <chrischen96@outlook.com>
2023-06-06 13:56:28 -0700

    finished server.js

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const cors = require('cor')
+const PORT = process.env.PORT || 3001
+const db = require('./db')
+const AppRouter = require('./routes/appRoutes.js')
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.listen(PORT, () => console(`Server running on port ${PORT}`))
+
+app.get('/', (req, res) => res.json({message: 'This is the home page'}))
+
+app.use('/api', AppRouter)
