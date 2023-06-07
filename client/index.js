5f1ae36437c4400dc72caef1062ea5e6274a5753
jlemenager <jeremy@unitedbyzero.com>
2023-06-07 16:46:32 -0400

    making axios work

--- client/index.js
+++ client/index.js
@@ -1,10 +1,18 @@
 const submitButton = document.querySelector('#submit')
 const searchBar = document.querySelector('input')
 const sony = document.querySelector('#sony')
+const infoList = document.querySelector('ul')
 
+//if (sony.style.backgroundColor === '#D9D9D9'){
     submitButton.addEventListener('click',async()=>{
-            await axios.get('https://localhost:3001/api/brands')
-            .then((res)=>console.log(res.data.brands))
-            .catch((e)=>console.log(e))
+        await axios.get('http://localhost:3001/api/products')
+        .then((res)=>{
+            console.log(res.data.products)
+            for(let i = 0;i<10;i++){
+                infoList.insertAdjacentHTML('afterbegin',`<li>${res.data.products[i].name}</li>`)
+            }
         })
+        .catch((e)=>console.log(e))
+    })
+//}
 
