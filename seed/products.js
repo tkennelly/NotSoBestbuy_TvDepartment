db87851e32b75c22d272324a5d160616b961e512
tkennelly <theresa.a.kennelly@gmail.com>
2023-06-07 10:37:45 -0400

    add partial products seed

--- seed/products.js
+++ seed/products.js
@@ -4,16 +4,67 @@ const { Product, Brand } = require('../models')
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
 const main = async () => {
-    
+    const brand1 = await Brand.find({ name: 'Samsung' })
+    const brand2 = await Brand.find({ name: 'LG' })
+    const brand3 = await Brand.find({ name: 'Sony' })
+    const brand4 = await Brand.find({ name: 'TCL' })
+    const brand5 = await Brand.find({ name: 'VIZIO' })
+
     const products = [
         {
-            name: '',
-            price: { type: Number, required: true },
-            description: { type: String, required: true },
-            brand: { type: Schema.Types.ObjectId, ref: 'brand'},
-            size: { type: String, required: true },
-            modelNum: { type: Number, required: true },
-            itemsInStock: { type: Number, required: true }
+            name: `Class Micro LED 4K Smart Tizen TV`,
+            price: 149999.99,
+            description: `Welcome to a bold new world of luxury entertainment: MICRO LED.With sound this powerful, you might wonder where all the speakers are hidden. Keep up with it all—all on one convenient screen, all in real-time with 4Vue.Your Hollywood experience turns larger than life with nearly infinite color gradations.
+            `,
+            brand: brand1[0].id,
+            size: `110"`,
+            modelNum: 3295483698,
+            itemsInStock: 10
           },
+          {
+            name: `Class Neo QLED 4k UHD Smart Tizen TV`,
+            price: 14999.99,
+            description: `Our best 4K picture offers lush visuals and crisp detail that create an unmatched cinematic experience.`,
+            brand: brand1[0].id,
+            size: `98"`,
+            modelNum: 3295486967,
+            itemsInStock: 9
+          },
+          {
+            name: `Class C2 Series OLED evo 4k UHD Smart webOS TV`,
+            price: 1299.99,
+            description: `The Award-Winning¹ LG OLED C2 Is Better Than Ever. The versatile LG OLED C Series has gotten even more impressive with brighter colors, a new, more advanced processor and a sleeker, slimmer bezel. Self-lit LG OLED pixels create beautiful picture quality with infinite contrast, perfect black and over a billion colors, now even brighter.`,
+            brand: brand2[0].id,
+            size: `55"`,
+            modelNum: 4629488887,
+            itemsInStock: 8
+          },
+          {
+            name: `Class UQ75 Series LED 4K UHD Smart webOS TV`,
+            price: 599.99,
+            description: `Dive into a world of free content, thanks to over 300+ free LG Channels¹ and personalized recommendations for everyone you share the remote with.`,
+            brand: brand2[0].id,
+            size: `70"`,
+            modelNum: 4629483409,
+            itemsInStock: 7
+          },
+          {
+            name: `Class BRAVIA XR X92 LED 4K UHD Full Array Smart Google TV`,
+            price: 9999.99,
+            description: `Bring your home theater to life with the ultra-large screen 100” class (99.5” diag.) BRAVIA XR X92 Full Array LED TV. Everything you watch becomes more detailed and immersive with true-to-life 4K HDR, powered by the all-new Cognitive Processor XR. Feel the intensity of the sun and experience all the stars of the night sky with Full Array LED and XR Contrast Booster.`,
+            brand: brand3[0].id,
+            size: `100"`,
+            modelNum: 4793729047,
+            itemsInStock: 6
+          },
+          {
+            name: `Class BRAVIA XR Z9K 8K HDR Mini LED Google TV`,
+            price: 7999.99,
+            description: `Pristine 8K resolution meets astounding Mini LED contrast for a next-level viewing experience, powered by the intelligent Cognitive Processor XR. Feel your senses get pushed to the limit with thousands of tiny, high-density Mini LEDs in precisely controlled lighting zones. Coupled with XR Backlight Master Drive, the BRAVIA XR Z9K delivers unprecedented brightness, color, and contrast that make your content feel impossibly real.`,
+            brand: brand3[0].id,
+            size: `85"`,
+            modelNum: 479372749,
+            itemsInStock: 5
+          }
     ]
 }
