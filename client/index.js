294d9064cbd1693f29e0c94936affb1a9f65e7d7
jlemenager <jeremy@unitedbyzero.com>
2023-06-07 14:01:58 -0400

    a few edits to index.html and index.js on client side

--- client/index.js
+++ client/index.js
@@ -2,6 +2,6 @@ const submitButton = document.querySelector('#submit')
 const searchBar = document.querySelector('input')
 
 submitButton.addEventListener('click',async()=>{
-    const lookup = await axios.get('https://localhost:3001/api')
+    const lookup = await axios.get('localhost:3001')
     console.log(lookup)
 })
