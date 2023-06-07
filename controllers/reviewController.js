cfb56df0a269d2619b4afc89c5b293e4984281a0
chrischen <chrischen96@outlook.com>
2023-06-07 09:33:43 -0700

    completed review routes and controller

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -1,29 +1,71 @@
 const { Review } = require('../models')
 
-const getAllReviews = async(req,res) =>{
+const createReview = async (req, res) => {
+    try {
+        const review = new Review(req.body)
+        await review.save()
+        return res.status(200).json({ review })
+    } catch (e) {
+        return res.status(500).send(e.message)
+    }
+}
+
+const getAllReviews = async (req, res) =>{
     try{
         const reviews = await Review.find()
         return res.status(200).json({ reviews })
     } catch(e){
-        res.status(500).send(e.message)
+        return res.status(500).send(e.message)
     }
 }
 
-const getReview = async(req,res) => {
+const getReview = async (req,res) => {
     try{
         const { id } = req.params
         const review = await Review.findById(id)
         if(brand){
             return res.status(200).json({ review })
         } else {
-            return res.status(400).send('Review does not exist')
+            return res.status(400).json({message:'Review does not exist.'})
         }
     } catch(e){
+        return res.status(500).send(e.message)
+    }
+}
+
+const updateReview = async (req, res) => {
+    try {
+        const {id} = req.params
+        const review = await Review.findByIdAndUpdate(id, req.body, {new: true} ) 
+        if (review) {
+            return res.status(200).json({review})
+        } else {
+            return res.status(400).json({message: 'Review does not exist.'})
+        }
+        
+    } catch (e) {
+        return res.status(500).send(e.message)
+    }
+}
+
+const deleteReview = async (req, res) => {
+    try {
+        const {id} = req.params
+        const review = await Review.findByIdAndDelete(id)
+        if (review) {
+            return res.status(200).json({review})
+        } else {
+            return res.status(400).json({message: 'Review does not exist.'})
+        }
+    } catch (e) {
         res.status(500).send(e.message)
     }
 }
 
 module.exports = {
+    createReview,
     getAllReviews,
-    getReview
+    getReview,
+    updateReview,
+    deleteReview
 }
--- routes/reviewRoute.js
+++ routes/reviewRoute.js
@@ -2,7 +2,10 @@ const express = require('express')
 const router = express.Router()
 const controller = require('../controllers/reviewController.js')
 
+router.post('/', controller.createReview)
 router.get('/', controller.getAllReviews)
 router.get('/:id', controller.getReview)
+router.push('/:id', controller.updateReview)
+router.delete('/:id', controller.deleteReview)
 
 module.exports = router
